04eb0709f663f50112f8ed31503e8d66577c5a89
Eldo Junior <eldo.junior@govoll.com>
2022-07-24 04:43:23 -0300

    fc

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = require('request-promise').defaults({ jar: true });
+const cheerio = require('cheerio');
+const {estimate, getWorldPop} = require('./world.pop');
+
+const uri =
+  'https://populationmatters.org/population-numbers?gclid=CjwKCAjwnZaVBhA6EiwAVVyv9GUkq1yurVjXe042wt7NVzhwvG3c2Ir7HpwE-rNx0IOK9VMIaOVG6BoCaxwQAvD_BwE';
+
+const config = {
+  uri,
+  transform: (body) => cheerio.load(body),
+};
+
+const getCurrentWorldPopulation = async () => {
+  const $ = await request(config);
+  const body = $('body').html();
+  const worldPop = getWorldPop();
+  const diffTo8Bi = new Intl.NumberFormat('pt-BR').format(
+    8000000000 - estimate()
+  );
+  const timeInDays = (8000000000 - estimate()) / 2.6 / 86400;
+  const timeInMonths = Math.floor(timeInDays / 30);
+  const diffDays1 = Math.floor((timeInDays / 30 - timeInMonths) * 30);
+  const diffWeeks = Math.floor(diffDays1 / 7);
+  const diffDays = Math.floor((diffDays1 / 7 - diffWeeks) * 7);
+  const diffHours1 = (((diffDays1 / 7 - diffWeeks) * 7) - diffDays) * 24;
+  const diffHours2 = (timeInDays - Math.floor(timeInDays)) * 24;
+  const diffHours = Math.floor(diffHours1 + diffHours2);
+  const diffMinutes = Math.floor((diffHours1 + diffHours2 - diffHours) * 60);
+  const diffSeconds = Math.floor(
+    ((diffHours1 + diffHours2 - diffHours) * 60 - diffMinutes) * 60
+  );
+
+  console.log(
+    `Current World Population ${worldPop} (+${diffTo8Bi} to 8BI) (${timeInMonths} months, ${diffWeeks} weeks, ${diffDays} days, ${diffHours} hours, ${diffMinutes} minutes, ${diffSeconds} seconds)`
+  );
+};
+
+(async () => {
+  setInterval(await getCurrentWorldPopulation, 5000);
+})();
